28918e16f91a878dac3c225f0da578784cd72e47
Luca Pisani <luca.pisani@abstract.it>
2016-09-02 12:44:17 +0200

    Update helpdesk.sh

--- crons/helpdesk.sh
+++ crons/helpdesk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+source /var/local/environment/vars
 wget -O - https://taskman.eionet.europa.eu/helpdesk_mailer/get_mail?key=$HELPDESK_EMAIL_KEY >> /var/local/redmine/log/helpdesk_cron.log
