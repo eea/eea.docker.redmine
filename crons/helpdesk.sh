b09e1439d48fc7972f7b9c397e7a928f783c4295
Anthony Monthe (ZuluPro) <anthony.monthe@eaudeweb.ro>
2016-11-01 16:04:14 +0100

    refs #75471 Send helpdesk cron's log to stdout

--- crons/helpdesk.sh
+++ crons/helpdesk.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 source /var/local/environment/vars
-echo "$(date) - $(wget -O - https://taskman.eionet.europa.eu/helpdesk_mailer/get_mail?key=$HELPDESK_EMAIL_KEY)" >> /home/redmine/redmine/log/helpdesk_cron.log
+
+if [[ -z "${LOG_FILE}" || ! -w "${LOG_FILE}" ]] ; then
+  LOG_FILE=/proc/1/fd/1
+fi
+
+echo "$(date) - helpdesk - $(wget -O - https://taskman.eionet.europa.eu/helpdesk_mailer/get_mail?key=$HELPDESK_EMAIL_KEY)" >> $LOG_FILE 2>&1
