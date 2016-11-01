82a83e2038832a315609e4a1042d0800a34fbe10
Anthony Monthe (ZuluPro) <anthony.monthe@eaudeweb.ro>
2016-11-01 16:07:33 +0100

    refs #75471 Send taskman_email.sh's logs to stdout

--- crons/taskman_email.sh
+++ crons/taskman_email.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 REDMINE_PATH=/var/local/redmine
-LOG_DIR=$REDMINE_PATH/log
 
 source /var/local/environment/vars
 
-/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
+if [[ -z "${LOG_FILE}" || ! -w "${LOG_FILE}" ]] ; then
+  LOG_FILE=/proc/1/fd/1
+fi
+
+echo "email - $(/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
 host=$T_EMAIL_HOST username=$T_EMAIL_USER password=$T_EMAIL_PASS ssl=$T_EMAIL_SSL port=$T_EMAIL_PORT folder=$T_EMAIL_FOLDER \
 project=it-helpdesk move_on_success=read move_on_failure=failed \
-allow_override=project,tracker,priority,status,category,fixed_version >> $LOG_DIR/taskman.log 2>&1
+allow_override=project,tracker,priority,status,category,fixed_version)" >> $LOG_FILE 2>&1
