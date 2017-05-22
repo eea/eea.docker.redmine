2e9b47c72c3a513a45d45600d64f3d1e11b6a81f
Zoltan Andras <zoli1884@gmail.com>
2017-05-22 16:34:46 +0300

    Refs #85184; Mailer issues cron script needed env paths to be set

--- crons/redmine_mailerissues.sh
+++ crons/redmine_mailerissues.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 REDMINE_PATH=/usr/src/redmine
 
@@ -6,4 +6,13 @@ if [[ -z "${LOG_FILE}" || ! -w "${LOG_FILE}" ]] ; then
   LOG_FILE=/proc/1/fd/1
 fi
 
-echo "mailerissues - $(/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile reminder:exec RAILS_ENV="production")" >> $LOG_FILE 2>&1
+cd $REDMINE_PATH
+
+export PATH=/usr/local/bin:$PATH
+export GEM_HOME=/usr/local/bundle
+export GEM_PATH=/usr/local/bundle/gems:/usr/local/lib/ruby/gems/2.2.0
+export BUNDLE_APP_CONFIG=/usr/local/bundle
+export BUNDLE_BIN=/usr/local/bundle/bin
+export BUNDLE_PATH=/usr/local/bundle
+
+echo "mailerissues - $(bin/rake -f Rakefile reminder:exec[test] RAILS_ENV=production)" >> $LOG_FILE 2>&1
