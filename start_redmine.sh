cd9d24e3fafcb90edf06ccd8921084f1dfcf0d45
valentinab25 <valentinab25>
2017-11-16 17:15:31 +0100

    Add TZ to cron defaults

--- start_redmine.sh
+++ start_redmine.sh
@@ -5,6 +5,7 @@ service cron restart
 
 echo "export SYNC_API_KEY=$SYNC_API_KEY"  >> ${REDMINE_PATH}/.profile
 echo "export SYNC_REDMINE_URL=$SYNC_REDMINE_URL"  >> ${REDMINE_PATH}/.profile 
+echo "TZ=$TZ" >> /etc/default/cron
 
 /docker-entrypoint.sh rails server -b 0.0.0.0
 
