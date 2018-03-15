a54f480e6e750bf106ae44643b3028dae5b645fb
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2018-03-15 16:59:53 +0200

    Revert to old version, to be used manually

--- config/install_plugins.sh
+++ config/install_plugins.sh
@@ -4,6 +4,8 @@ if [ -d "/install_plugins" ]; then
    for i in /install_plugins/*.zip; do
        unzip -d ${REDMINE_PATH}/plugins -o $i
    done
-   touch ${REDMINE_PATH}/log/redmine_helpdesk.log
+   #install plugins dependencies
+   /usr/local/bin/bundle install --without development test
+   /usr/local/bin/bundle exec rake redmine:plugins:migrate
    chown redmine:redmine ${REDMINE_PATH}/log/redmine_helpdesk.log
 fi
