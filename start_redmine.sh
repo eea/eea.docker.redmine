d26c6952ce0f5e640c144a2ec4bfdb09d424385a
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2018-03-15 16:58:32 +0200

    Move install_plugins.sh code to start_redmine.sh

--- start_redmine.sh
+++ start_redmine.sh
@@ -40,39 +40,36 @@ H_EMAIL_SSL=false" > /var/local/environment/vars
 fi
 
 if [ ! -z "${PLUGINS_URL}" ]; then
-  run_install=0
   read_conf=0
   for plugin_conf in $(cat ${REDMINE_PATH}/plugins.cfg); do
       read_conf=$(( $read_conf + 1 ))  
       if  (( read_conf % 2 )); then
           if [ ! -d ${REDMINE_PATH}/plugins/$plugin_conf ]; then
             echo "Found missing plugin - $plugin_conf, will install it"
-            run_install=1
+            REDMINE_PLUGINS_MIGRATE="yes"
          fi   
       else
           if [ ! -f /install_plugins/$plugin_conf ]; then
               echo "Found missing plugin - $plugin_conf, will download and install it"
               full_url=${PLUGINS_URL/https:\/\//https:\/\/$PLUGINS_USER:$PLUGINS_PASSWORD@}
               wget -q -O  /install_plugins/$plugin_conf $full_url/$plugin_conf
-              run_install=1
+              unzip -d ${REDMINE_PATH}/plugins -o /install_plugins/$plugin_conf
+              REDMINE_PLUGINS_MIGRATE="yes" 
           fi
       fi
   done
 
   #remove old plugins
   for file in  /install_plugins/*; do 
-    if [ $(grep  ${file/\/install_plugins\//} ${REDMINE_PATH}/plugins.cfg | wc -l ) -eq 0 ]; then
+    if [ $(grep  " ${file/\/install_plugins\//}$" ${REDMINE_PATH}/plugins.cfg | wc -l ) -eq 0 ]; then
          rm $file
-    fi
-    
-    if [ ! -d ${REDMINE_PATH}/plugins/$(echo $plugin | cut -d'-' -f1) ]; then
-         run_install=1
-    fi
+    fi  
   done 
 
-  if [ $run_install -eq 1 ]; then
-     ${REDMINE_PATH}/install_plugins.sh
-     export REDMINE_PLUGINS_MIGRATE=yes
+  if [[ $REDMINE_PLUGINS_MIGRATE == "yes" ]]; then
+         touch ${REDMINE_PATH}/log/redmine_helpdesk.log
+         chown redmine:redmine ${REDMINE_PATH}/log/redmine_helpdesk.log
+         export REDMINE_PLUGINS_MIGRATE
   fi
 
 fi
