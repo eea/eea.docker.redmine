7f59d5d6fbe5ce0c4dd7222fcaa5838c3d9a030b
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-05-04 13:30:47 +0300

    Fix email script

--- crons/taskman_email.sh
+++ crons/taskman_email.sh
@@ -14,7 +14,9 @@ fi
 
 cd $REDMINE_PATH
 
-echo "email - $(/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
+echo "email - "
+
+/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:email:receive_imap RAILS_ENV="production" \
 host=$T_EMAIL_HOST username=$T_EMAIL_USER password=$T_EMAIL_PASS ssl=$T_EMAIL_SSL port=$T_EMAIL_PORT folder=$T_EMAIL_FOLDER \
 project=it-helpdesk move_on_success=read move_on_failure=failed \
-allow_override=project,tracker,priority,status,category,fixed_version)"
+allow_override=project,tracker,priority,status,category,fixed_version
