1090813bdf3a06ff0a15307a38eac0a36f00dd59
valentinab25 <valentinab25>
2020-11-25 23:53:14 +0200

    deprecate dalli_store

--- config/additional_environment.rb
+++ config/additional_environment.rb
@@ -1,9 +1,8 @@
    config.logger = Logger.new(STDOUT)
    config.logger.level = Logger::INFO
 
-   config.gem 'dalli'
    config.action_controller.perform_caching  = true
    config.cache_classes = true
-   config.cache_store = :dalli_store, "memcached:11211"
-   config.action_controller.cache_store = :dalli_store, "memcached:11211"
-   config.redmine_search_cache_store = :dalli_store, "memcached:11211"
+   config.cache_store = :mem_cache_store, "memcached:11211"
+   config.action_controller.cache_store = :mem_cache_store, "memcached:11211"
+   config.redmine_search_cache_store = :mem_cache_store, "memcached:11211"
