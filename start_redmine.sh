965cb1f16c40cb428263c5adfa363f966706f0ab
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2023-04-19 15:57:41 +0300

    add GITHUB_AUTHENTICATION variable

--- start_redmine.sh
+++ start_redmine.sh
@@ -13,6 +13,7 @@ service cron restart
 
 echo "export SYNC_API_KEY=$SYNC_API_KEY"  >> ${REDMINE_PATH}/.profile
 echo "export SYNC_REDMINE_URL=$SYNC_REDMINE_URL"  >> ${REDMINE_PATH}/.profile 
+echo "export GITHUB_AUTHENTICATION=$GITHUB_AUTHENTICATION"  >> ${REDMINE_PATH}/.profile 
 echo "TZ=$TZ" >> /etc/default/cron
 
 if [ -n "${T_EMAIL_HOST}" ]; then
