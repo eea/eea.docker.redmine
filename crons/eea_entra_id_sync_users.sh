c496c04d05a919d5627edfb1d4288b4a65d240a7
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2025-08-07 15:24:23 +0300

    Added the cronjobs for Entra ID sync.

---
+++ crons/eea_entra_id_sync_users.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REDMINE_PATH=/usr/src/redmine
+
+if [ -e /var/local/environment/vars ]; then
+  source /var/local/environment/vars
+fi
+
+if [ -e $REDMINE_PATH/.profile ]; then
+  source $REDMINE_PATH/.profile
+fi
+
+export GEM_HOME=/usr/local/bundle
+export BUNDLE_APP_CONFIG=/usr/local/bundle
+unset BUNDLE_PATH
+
+cd $REDMINE_PATH
+echo "ldapsync - "
+
+/usr/local/bin/bundle exec rake -f $REDMINE_PATH/Rakefile redmine:plugins:entra_id:sync:users RAILS_ENV="production"
