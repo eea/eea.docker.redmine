7dc17a220c80825a5a0f7c0c943866874104809f
Alexandru Ghica <alecghica@gmail.com>
2026-03-24 23:32:18 +0200

    Addons update

--- config/build/install_core_plugins.sh
+++ config/build/install_core_plugins.sh
@@ -22,8 +22,8 @@ git clone -b 0.3.5 https://github.com/agileware-jp/redmine_banner.git "${REDMINE
 git clone https://github.com/enricohuang/redmine_mermaid.git "${REDMINE_PATH}/plugins/redmine_mermaid"
 git clone -b 3.4.0 https://github.com/alphanodes/additionals.git "${REDMINE_PATH}/plugins/additionals"
 sed -i "s#require 'additionals/plugin_version'#require_relative 'lib/additionals/plugin_version'#" "${REDMINE_PATH}/plugins/additionals/init.rb"
-git clone -b v1.5.1 https://github.com/mikitex70/redmine_drawio.git "${REDMINE_PATH}/plugins/redmine_drawio"
-git clone -b 1.0.7 https://github.com/ncoders/redmine_local_avatars.git "${REDMINE_PATH}/plugins/redmine_local_avatars"
+git clone -b v1.5.2 https://github.com/mikitex70/redmine_drawio.git "${REDMINE_PATH}/plugins/redmine_drawio"
+git clone -b 1.1.0 https://github.com/ncoders/redmine_local_avatars.git "${REDMINE_PATH}/plugins/redmine_local_avatars"
 
 git clone https://github.com/eea/redmine_xls_export.git "${REDMINE_PATH}/plugins/redmine_xls_export"
 cd "${REDMINE_PATH}/plugins/redmine_xls_export"
@@ -31,4 +31,4 @@ git checkout 087afa403b34a32313e7761cd018879f05f19e3c
 cd ..
 
 git clone -b master https://github.com/eea/redmine_entra_id.git "${REDMINE_PATH}/plugins/entra_id"
-git clone -b 1.11.0 https://github.com/haru/redmine_ai_helper.git "${REDMINE_PATH}/plugins/redmine_ai_helper"
+git clone -b 2.4.0 https://github.com/haru/redmine_ai_helper.git "${REDMINE_PATH}/plugins/redmine_ai_helper"
