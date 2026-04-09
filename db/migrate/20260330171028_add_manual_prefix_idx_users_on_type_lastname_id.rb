7340b542f883df9009046b4b8f12f60141e5fe29
Silviu <silviupanaite@users.noreply.github.com>
2026-04-09 16:27:36 +0300

    Add manual prefix index migrations for users and issues

---
+++ db/migrate/20260330171028_add_manual_prefix_idx_users_on_type_lastname_id.rb
@@ -0,0 +1,58 @@
+class AddManualPrefixIdxUsersOnTypeLastnameId < ActiveRecord::Migration["#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}".to_f]
+  disable_ddl_transaction!
+
+  TABLE_NAME = :users
+  INDEX_COLUMNS = [:type, :lastname, :id].freeze
+  INDEX_NAME = "manual_prefix_idx_users_on_type_lastname_id"
+
+  def up
+    unless table_exists?(TABLE_NAME)
+      say "[manual_indexes] skip #{INDEX_NAME}: table #{TABLE_NAME} does not exist", true
+      return
+    end
+
+    if index_exists?(TABLE_NAME, INDEX_COLUMNS, name: INDEX_NAME) || index_name_exists?(TABLE_NAME, INDEX_NAME)
+      say "[manual_indexes] skip #{INDEX_NAME}: already exists", true
+      return
+    end
+
+    add_index_with_fallback
+    say "[manual_indexes] created #{INDEX_NAME}", true
+  rescue StandardError => e
+    say "[manual_indexes] soft-fail #{INDEX_NAME}: #{e.class}: #{e.message}", true
+  end
+
+  def down
+    remove_index TABLE_NAME, name: INDEX_NAME, if_exists: true
+  end
+
+  private
+
+  def add_index_with_fallback
+    if mysql_adapter?
+      add_mysql_online_index
+    else
+      add_index(TABLE_NAME, INDEX_COLUMNS, name: INDEX_NAME)
+    end
+  end
+
+  def mysql_adapter?
+    connection.adapter_name.to_s.downcase.include?("mysql")
+  end
+
+  def add_mysql_online_index
+    quoted_columns = INDEX_COLUMNS.map { |column| connection.quote_column_name(column) }.join(", ")
+    quoted_table = connection.quote_table_name(TABLE_NAME)
+    quoted_name = connection.quote_column_name(INDEX_NAME)
+
+    execute <<~SQL.squish
+      ALTER TABLE #{quoted_table}
+      ADD INDEX #{quoted_name} (#{quoted_columns}),
+      ALGORITHM=INPLACE,
+      LOCK=NONE
+    SQL
+  rescue ActiveRecord::StatementInvalid => e
+    say "[manual_indexes] online DDL failed for #{INDEX_NAME}: #{e.message}; retrying regular add_index", true
+    add_index(TABLE_NAME, INDEX_COLUMNS, name: INDEX_NAME)
+  end
+end
