69ac00da692ccf35935ccf01da4c177ec89a6354
Silviu <silviupanaite@users.noreply.github.com>
2026-05-06 17:03:28 +0300

    fix: align plugin ID with directory name zzzz_eea_patches
    - Plugin ID changed from :redmine_eea_patches to :zzzz_eea_patches
    - Version bumped to 1.1.0
    - Improved logging for debugging

--- plugins/zzzz_eea_patches/init.rb
+++ plugins/zzzz_eea_patches/init.rb
@@ -1,19 +1,15 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_eea_patches do
+Redmine::Plugin.register :zzzz_eea_patches do
   name 'EEA Performance Patches'
   author 'European Environment Agency (EEA)'
   description 'Performance optimizations and patches for Redmine plugins'
-  version '1.0.0'
+  version '1.1.0'
   url 'https://github.com/eea/eea.docker.redmine'
   author_url 'https://www.eea.europa.eu'
 
-  # Minimum Redmine version
   requires_redmine version_or_higher: '5.0.0'
-
-  # Must load AFTER redmine_contacts_helpdesk so view overrides take effect
   requires_redmine_plugin :redmine_contacts_helpdesk, :version_or_higher => '4.0.0'
 end
 
-# Log that the plugin is loaded
-Rails.logger.info '[redmine_eea_patches] Plugin loaded successfully'
+Rails.logger.info '[zzzz_eea_patches] Plugin registered, dir=zzzz_eea_patches'
