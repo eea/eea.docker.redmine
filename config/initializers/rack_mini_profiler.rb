7204133946a0e196566161de9b261f26e80552a0
Silviu <silviupanaite@users.noreply.github.com>
2026-05-14 18:50:29 +0300

    fix: gate mini profiler behind admin session toggle

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -1,16 +1,29 @@
 enabled = ENV.fetch("RACK_MINI_PROFILER_ENABLED", "0") == "1"
+session_toggle_key = :mini_profiler_enabled
 
 return unless defined?(Rack::MiniProfiler)
 
 profiler_authorized = lambda do |env|
   begin
-    return true if ENV.fetch("RACK_MINI_PROFILER_ALLOW_ALL", "0") == "1"
-
     request = ActionDispatch::Request.new(env)
     session = request.session
+
+    # Admin-only runtime toggle via URL for current session:
+    #   ?miniprofiler=on  -> enable profiling
+    #   ?miniprofiler=off -> disable profiling
+    toggle = request.params["miniprofiler"].to_s.downcase
+    if toggle == "on"
+      session[session_toggle_key] = true
+    elsif toggle == "off"
+      session.delete(session_toggle_key)
+    end
+
+    # Escape hatch for ops when explicitly requested
+    return true if ENV.fetch("RACK_MINI_PROFILER_ALLOW_ALL", "0") == "1"
+
     user_id = session && session[:user_id]
     user = defined?(User) ? User.find_by(id: user_id) : nil
-    user&.admin? || false
+    user&.admin? && session[session_toggle_key] == true
   rescue StandardError
     false
   end
@@ -18,12 +31,12 @@ end
 
 apply_config = lambda do
   Rack::MiniProfiler.config.auto_inject = enabled
+  Rack::MiniProfiler.config.authorization_mode = :allow_authorized
   if Rack::MiniProfiler.config.respond_to?(:enable_hotwire_turbo_drive_support=)
     Rack::MiniProfiler.config.enable_hotwire_turbo_drive_support = true
   end
 
   if enabled
-    Rack::MiniProfiler.config.authorization_mode = :allow_all
     Rack::MiniProfiler.config.pre_authorize_cb = profiler_authorized
   else
     Rack::MiniProfiler.config.pre_authorize_cb = ->(_env) { false }
